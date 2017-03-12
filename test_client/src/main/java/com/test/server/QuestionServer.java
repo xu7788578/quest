efc9e0d690e0e2d23952d1d418082de0887d3987
xu7788578 <793307165@qq.com>
2017-03-12 20:14:30 +0800

    3

--- test_client/src/main/java/com/test/server/QuestionServer.java
+++
@@ -1,8 +0,0 @@
-package com.test.server;
-
-import com.test.request.UserApiRequest;
-
-public interface QuestionServer {
-   public int getRanDom();
-   public Boolean insert(UserApiRequest user);
-}
--- test_impl/src/main/java/com/test/dao/UserDao.java
+++
@@ -1,7 +0,0 @@
-package com.test.dao;
-
-import com.test.model.request.User;
-
-public interface UserDao {
- public Boolean insert(User user);
-}
